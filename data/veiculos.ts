549ab22bc7ac938453bbdaa94483db7616277656
Carolina Lansoni Vilas Boas <161716386+lvbcarol@users.noreply.github.com>
2025-10-29 19:23:25 -0300

    ajuste veiculos.ts

--- data/veiculos.ts
+++ data/veiculos.ts
@@ -1,6 +1,25 @@
 
 // data/veiculos.ts - ARQUIVO GERADO AUTOMATICAMENTE (Total: 213 veículos)
 
+export interface FichaTecnica {
+  motor: string;
+  potencia: string;
+  torque: string;
+  transmissao: string;
+  pesoEmOrdemDeMarcha: string;
+  pbtTecnico: string;
+  cmt: string; 
+}
+
+export interface Veiculo {
+  id: string;
+  nome: string;
+  modelo: string;
+  imagem: string;
+  resumoVantagem: string;
+  fichaTecnica: FichaTecnica;
+}
+
 // ... (Resto do código de interface e export)
 export const VeiculosData: Veiculo[] = [
   {
